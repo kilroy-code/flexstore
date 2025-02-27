53b7c33994fdf77e5d9246f2632f159156419a03
Howard Stearns <howard.stearns@gmail.com>
2025-02-26 16:51:01 -0800

    better loading semantics, with this.persist instance being a promise

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import Credentials from '@ki1r0y/distributed-security';
 import Synchronizer from './lib/synchronizer.mjs';
-import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, ready } from  './lib/collections.mjs';
+import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection } from  './lib/collections.mjs';
 
-export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, ready };
-export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, ready };
+export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer };
+export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer };
