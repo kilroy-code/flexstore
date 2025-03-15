28db5a8fbe6d8edd7aa0d8d043556c6fcfcc745f
Howard Stearns <howard.stearns@gmail.com>
2025-03-14 17:16:12 -0700

    say where you came from

--- index.mjs
+++ index.mjs
@@ -2,5 +2,7 @@ import Credentials from '@ki1r0y/distributed-security';
 import Synchronizer from './lib/synchronizer.mjs';
 import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection } from  './lib/collections.mjs';
 
+console.log('flexstore from', import.meta.url);
+
 export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer };
 export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer };
