ec1b594dc92a01e54a184753c6a7df5c4139926b
Howard Stearns <howard.stearns@gmail.com>
2025-04-13 17:32:39 -0700

    checkpoint: multiplex off; channelId starts at 10; null initial channel id is recorded as 1

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import Credentials from '@ki1r0y/distributed-security';
 import Synchronizer from './lib/synchronizer.mjs';
 import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection } from  './lib/collections.mjs';
-import { WebRTC, PromiseWebRTC } from './lib/webrtc.mjs';
+import { WebRTC, PromiseWebRTC, SharedWebRTC } from './lib/webrtc.mjs';
 
 console.log('flexstore from', import.meta.url);
 
-export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC };
-export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC };
+export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC };
+export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC };
