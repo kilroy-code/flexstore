0fbd9304fc2175f697e96e8c77187df2355f7ec7
Howard Stearns <howard.stearns@gmail.com>
2025-06-11 12:24:38 -0700

    test-server for command-line regression tests

---
+++ test-server.mjs
@@ -0,0 +1,18 @@
+import process from 'process';
+import express from 'express';
+import logger from 'morgan';
+import compression from 'compression';
+import flexstoreRouter from '@kilroy-code/flexstore/router';
+
+process.title = 'ki1r0y-test-server';
+
+const port = 3000;
+const app = express();
+// It is more efficient to support gzip in a production reverse proxy, but doing it here in development
+// so that we're more likely hit any snags now rather than in production.
+app.use(compression());
+app.use(logger('dev'));
+app.use('/flexstore', flexstoreRouter); // Before any json parser.
+app.listen(port, () => console.log(`Listening on port ${port}.`));
+
+
