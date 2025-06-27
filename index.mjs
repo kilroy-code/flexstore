c3c550139c434c6a446cb560aa7306fcf9449003
Howard Stearns <howard.stearns@gmail.com>
2025-06-26 18:01:56 -0700

    expose uuid4, Synchronizer.fetchJSON; change subscribe PUT to POST

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import Credentials from '@ki1r0y/distributed-security';
+import uuid4 from 'uuid4';
 import Synchronizer from './lib/synchronizer.mjs';
-import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection } from  './lib/collections.mjs';
+import { Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, StorageLocal } from  './lib/collections.mjs';
 import { WebRTC, PromiseWebRTC, SharedWebRTC } from './lib/webrtc.mjs';
 import { version, name, storageVersion, storageName } from './lib/version.mjs';
 
 console.log(`${name} ${version} from ${import.meta.url}.`);
 
-export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC, name, version, storageName, storageVersion };
-export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC, name, version,  storageName, storageVersion };
+export { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC, name, version, storageName, storageVersion, StorageLocal, uuid4 };
+export default { Credentials, Collection, ImmutableCollection, MutableCollection, VersionedCollection, VersionCollection, Synchronizer, WebRTC, PromiseWebRTC, SharedWebRTC, name, version,  storageName, storageVersion, StorageLocal, uuid4 };
