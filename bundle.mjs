186184004fa5aa95fc4ba7fe930350470efe51ca
Howard Stearns <howard.stearns@gmail.com>
2025-07-03 14:17:14 -0700

    fix version/bundle?

--- bundle.mjs
+++ bundle.mjs
@@ -337,7 +337,7 @@ class SharedWebRTC extends PromiseWebRTC {
 }
 
 var name$1 = "@kilroy-code/flexstore";
-var version$1 = "0.0.54";
+var version$1 = "0.0.55";
 var _package = {
 	name: name$1,
 	version: version$1};
